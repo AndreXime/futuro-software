850ab7eade4422b692baca18a2dceaeeefd506eb
AndreXime <andreximenesa20@gmail.com>
2025-03-28 22:10:46 -0300

    FIX: Title do site e seção about no mobile

--- src/components/HomePage/About.tsx
+++ src/components/HomePage/About.tsx
@@ -1,17 +1,15 @@
 export default function AboutSection() {
 	return (
-		<section className="relative py-16 bg-base-100 bg-opacity-0 text-center">
+		<section className="relative py-16 bg-base-100 bg-opacity-0 text-center p-8">
 			<div className="max-w-4xl mx-auto">
 				<h2 className="text-center text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
 					Sobre a Futuro Software
 				</h2>
 
 				<p className="mt-4 text-gray-600">
-					A Futuro Software é uma empresa de tecnologia inovadora, especializada em criar soluções digitais
-					personalizadas para empresas de todos os portes. Nosso compromisso é oferecer software de alta performance que
-					impulsiona a transformação digital, otimizando processos e ampliando resultados. Estamos sempre em busca de
-					novas formas de revolucionar o mercado, utilizando tecnologia de ponta para criar produtos que atendem às
-					necessidades dos nossos clientes.
+					Nosso compromisso é oferecer software de alta performance que impulsiona a transformação digital, otimizando
+					processos e ampliando resultados. Estamos sempre em busca de novas formas de revolucionar o mercado,
+					utilizando tecnologia de ponta para criar produtos que atendem às necessidades dos nossos clientes.
 				</p>
 			</div>
 		</section>
